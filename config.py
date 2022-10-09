3f4872a0b0f86aaa9f0898f5df8366500bddb4eb
Azat Ismagilov <azatismagilov00@gmail.com>
2022-10-09 00:07:37 +0000

    Use delimeter from config and normal column names

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 csv_path = 'teams.csv'
+delimeter = ';'
+
 group_images_directory = 'group_photos'
 group_output_directory = 'output_group'
 tags_file = 'tags.txt'
--- library/team.py
+++ library/team.py
@@ -4,6 +4,8 @@ from typing import List
 
 import csv
 
+import config
+
 @dataclass
 class Participant:
     name: str
@@ -17,9 +19,9 @@ class Team:
 def parse_teams_from_csv(csv_path) -> List[Team]:
     team_participants = defaultdict(list)
     with open(csv_path, 'r') as csv_file:
-        reader = csv.DictReader(csv_file, delimiter=';')
+        reader = csv.DictReader(csv_file, delimiter=config.delimeter)
         for row in reader:
-            team_participants[row['instName']].append(Participant(row['Name'], None))
+            team_participants[row['team']].append(Participant(row['name'], None))
 
     return [Team(team_name, participants)
             for team_name, participants in team_participants.items()

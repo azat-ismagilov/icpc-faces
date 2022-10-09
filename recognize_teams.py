7de5e969930c83bcd0df1d950a277733ff63ccbd
Azat Ismagilov <azatismagilov00@gmail.com>
2022-10-09 16:44:46 +0300

    Add default face-info saver

--- recognize_teams.py
+++ recognize_teams.py
@@ -10,9 +10,16 @@ from library.teams_processor import process_teams
 def main():
     start = time.time()
 
-    image_paths = find_photos_by_tag(config.group_images_directory)
-    images = [GroupImageProcess(image_path) for image_path in tqdm(image_paths, desc="Face detection and ocr")]
-    print("Processed all images: {}".format(time.time() - start))
+    try:
+        with open('save.pkl', 'rb') as f:
+            images = pickle.load(f)
+        print('You are using precalculated images info. If you want to update your images, please delete save.pkl')
+    except:
+        image_paths = find_photos_by_tag(config.group_images_directory)
+        images = [GroupImageProcess(image_path) for image_path in tqdm(image_paths, desc="Face detection and ocr")]
+        print("Processed all images: {}".format(time.time() - start))
+        with open('save.pkl', 'wb') as f:
+            pickle.dump(images, f)
 
     process_teams(config.csv_path, config.group_output_directory, config.tags_file, images)
     print("Time elapsed: {}".format(time.time() - start))

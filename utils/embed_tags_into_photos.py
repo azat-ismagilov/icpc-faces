1a3e2b7aa98e723811424f69b044d6dfd8c92c11
Azat Ismagilov <azatismagilov00@gmail.com>
2022-10-11 22:38:30 +0000

    Add tool to embed tags into photos

---
+++ utils/embed_tags_into_photos.py
@@ -0,0 +1,35 @@
+import argparse
+import traceback
+import csv
+
+from exiftool import ExifToolHelper
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Embed tags from file to images')
+    parser.add_argument('tags_file', type=str,
+                        help='Tags file', default='tags.txt', nargs='?')
+    args = parser.parse_args()
+
+    et = ExifToolHelper()
+    with open(args.tags_file, 'r', encoding='utf-8') as f:
+        rd = csv.reader(f, delimiter='\t', quotechar='"', lineterminator='\n')
+        for row in rd:
+            try:
+                path = row[0]
+                tags = row[1:]
+
+                for d in et.get_tags(path, tags=['IPTC:Keywords']):
+                    tags = d['IPTC:Keywords'] + tags
+
+                tags = list(dict.fromkeys(tags))
+
+                et.set_tags(path, tags={
+                    'Keywords': tags,
+                })
+            except Exception:
+                traceback.print_exc()
+
+
+if __name__ == '__main__':
+    main()

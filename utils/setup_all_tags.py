e4892e36cc8838a806a05140409dab2fe5fc085d
Azat Ismagilov <azatismagilov00@gmail.com>
2022-10-15 21:59:13 +0000

    Add full setup

---
+++ utils/setup_all_tags.py
@@ -0,0 +1,39 @@
+import argparse
+import csv
+import traceback
+
+from methods import *
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Get tags from path',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        'dir', type=str, help='Input dir images', default='./', nargs='?')
+    parser.add_argument('tags_file', type=str,
+                        help='Name of the output file', default='tags.txt', nargs='?')
+    args = parser.parse_args()
+
+    with open(args.tags_file, 'w', encoding='utf-8') as f:
+        writer = csv.writer(f, delimiter='\t', quotechar='"',
+                            lineterminator='\n', quoting=csv.QUOTE_ALL)
+        for path in find_photos_in_directory(args.dir):
+            try:
+                tags = get_tags_from_photo(path) + \
+                    get_tags_from_path(args.dir, path) + \
+                    convert_digiKam_tags_to_picasa(path)
+
+                tags = list(dict.fromkeys(tags))
+
+                embed_tags_into_photo(path, tags)
+                embed_picasa_as_digiKam(path, tags)
+                set_description(path, tags)
+                writer.writerow([path] + tags)
+            except Exception:
+                print(path)
+                traceback.print_exc()
+
+
+if __name__ == '__main__':
+    main()
